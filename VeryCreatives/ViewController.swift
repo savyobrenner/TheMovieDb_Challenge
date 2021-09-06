bae9506adf7c4e95b709a98365d782b15854dc98
Savyo Brenner <savyobrenner@gmail.com>
2021-09-06 11:41:27 -0300

    Initial Commit

---
+++ VeryCreatives/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VeryCreatives
+//
+//  Created by Brenner on 06/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
