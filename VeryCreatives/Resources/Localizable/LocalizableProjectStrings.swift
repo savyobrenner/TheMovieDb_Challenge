23d14e40cda8a0b1f2c100bbc1d4e1c414fb479a
Savyo Brenner <savyobrenner@gmail.com>
2021-09-06 13:21:01 -0300

    add localizable files

---
+++ VeryCreatives/Resources/Localizable/LocalizableProjectStrings.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizableProjectStrings.swift
+//  Created by Brenner on 19/10/20.
+//  Copyright © 2020 Brenner. All rights reserved.
+//
+
+import Foundation
+
+enum ErrorMessages: String, Localizable {
+    case noConnection = "ErrorMessages_noConnection"
+    case genericError = "ErrorMessages_genericError"
+}
+
+enum BaseViewControllerStrings: String, Localizable {
+    case title = "BaseViewControllerStrings_title"
+    case button = "BaseViewControllerStrings_btn"
+}
